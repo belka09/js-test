ea1762fb2546831922f83252a45a5bc42dffefb8
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-01-15 15:26:30 +0100

    more test scenarios

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -24,3 +24,26 @@ it('should yeild a correct sum if an array of numeric string values is provided'
     const expectedResult = numbers.reduce((prevValue, nextValue) => +prevValue + +nextValue, 0);
     expect(result).toBe(expectedResult);
 });
+
+it('should yeild 0 if an empty array is provided', () => {
+    const numbers = [];
+    const result = add(numbers);
+    expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into a function', () => {
+    const resultFn = () => {
+        add();
+    }
+    expect(resultFn).toThrow();
+});
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    const resultFn = () => {
+        add(num1, num2);
+    }
+    expect(resultFn).toThrow();
+});
