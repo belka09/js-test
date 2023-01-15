50d47dc9acb12ceb15e3f9d8b9b51711fb35f230
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-01-15 14:51:45 +0100

    negative scenarios

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -11,3 +11,16 @@ it('should summarize all number values in array', () => {
     const expectedResult = numbers.reduce((prevValue, nextValue) => prevValue + nextValue, 0);
     expect(result).toBe(expectedResult);
 });
+
+it('should yeild NaN if a least one invalid number is provided', () => {
+    const inputs = ['invalid', 2, 3];
+    const result = add(inputs);
+    expect(result).toBeNaN();
+});
+
+it('should yeild a correct sum if an array of numeric string values is provided', () => {
+    const numbers = ['1', '2', '3'];
+    const result = add(numbers);
+    const expectedResult = numbers.reduce((prevValue, nextValue) => +prevValue + +nextValue, 0);
+    expect(result).toBe(expectedResult);
+});
