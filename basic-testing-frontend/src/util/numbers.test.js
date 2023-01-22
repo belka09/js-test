43962c54424127667b68d79332d449c7354b96b3
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-01-22 12:40:44 +0100

    add additional test for NaN case

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -7,6 +7,13 @@ it('should transform string number to number', () => {
     expect(result).toBeTypeOf('number');
 });
 
+it('should transform string number to number without NaN option', () => {
+    // transformToNumber() can return NaN object which is also type Number. We should need to test proper value without NaNN
+    const input = '1';
+    const result = transformToNumber(input);
+    expect(result).toBe(+input);
+});
+
 it('should yeild NaN for non-transformable values', () => {
     const input = 'invalid';
     const result = transformToNumber(input);
