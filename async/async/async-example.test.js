4b72601f97ee39a6ad171c8d6a1495df9a94410d
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-03-26 17:22:24 +0200

    test function with a callback

---
+++ async/async/async-example.test.js
@@ -0,0 +1,15 @@
+import { it, expect } from 'vitest';
+import { generateToken } from './async-example';
+
+it('should generate a token value', (done) => {
+    const testUserEmail = "test@test.com";
+
+    generateToken(testUserEmail, (err, token) => {
+        try {
+            expect(token).toBeDefined();
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
