3d3ff3f5f48c895f5e3afc83e90bef140e824e0e
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-03-26 18:53:02 +0200

    add spies

--- mocking/src/util/io.test.js
+++ mocking/src/util/io.test.js
@@ -3,4 +3,8 @@ import { promises as fs } from 'fs';
 
 import writeData from './io';
 
-
+it('should execute the writeFile method', () => {
+  const testData = 'test';
+  const testFilename = 'test.txt';
+  return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+});
