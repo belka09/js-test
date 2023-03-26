083fcfabf428f81680e6ddd8d8b3112fcdeeaab4
Dmitry Zimnitsky <dmitryft4@gmail.com>
2023-03-26 16:51:09 +0200

    use toEqual instead of toBe

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -30,7 +30,7 @@ describe('cleanNumbers()', () => {
     it('should return an array of number value if an array of string number values is provided', () => {
         const numberValues = ['1', '2'];
         const cleanedNumbers = cleanNumbers(numberValues);
-        expect(cleanedNumbers[0]).toBeTypeOf('number');
+        expect(cleanedNumbers).toEqual([1, 2]);
     });
 
     it('should throw an error if an array with at least one empty sting is provided', () => {
